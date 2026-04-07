4b5574717dc7b22e549b4a019148a07449065405
Gareth Chng <goofygoobers20230313@gmail.com>
2026-04-07 13:06:05 +0800

    im doing something

--- json_data/parse.py
+++ json_data/parse.py
@@ -1,9 +1,18 @@
 import json
 import csv
 
-with open("Booth Data.csv", "r") as file:
-    csv_reader = csv.DictReader(file)
-    booths = {row["booth_id"]: row for row in csv_reader}
+with open("Funtasia Booth.csv", "r") as file:
+    data = list(csv.DictReader(file))
 
-with open("booths.json", "w") as file:
-    json.dump(booths, file, indent=2)
+print(data)
+for i in data:
+    tmp = i["Zone"]
+    # print(tmp)
+    z,_ = tmp.split("-",1)
+    i["Zone"] = z
+
+length = len(data)
+json_data = {"l1":data[:length//3], "l2":data[length//3:2*length//3], "l3":data[2*length//3:]}
+
+with open("funtasia_data.json", "w") as file:
+    json.dump(json_data, file, indent=2)
